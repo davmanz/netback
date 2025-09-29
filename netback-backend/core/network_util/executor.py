ba1e76fd5a2e3b6e26a04ed38f340de492478e83
David Manzano <davidjosemanzano@gmail.com>
2025-09-29 14:54:50 -0500

    Correcion de inconsistencias en Vaultcredential

--- netback-backend/core/network_util/executor.py
+++ netback-backend/core/network_util/executor.py
@@ -2,14 +2,14 @@ from netmiko import ConnectHandler
 
 
 def executeCommandOnDevice(device, command):
-    if not device.customUser and not device.VaultCredential:
+    if not (device.customUser and device.customPass) and not device.vaultCredential:
         return "No credentials found for this device"
 
     connection = {
         "device_type": device.manufacturer.netmiko_type or "generic",
         "host": device.ipAddress,
-        "username": device.customUser or device.VaultCredential.username,
-        "password": device.customPass or device.VaultCredential.get_plain_password(),
+        "username": device.customUser or device.vaultCredential.username,
+        "password": device.customPass or device.vaultCredential.get_plain_password(),
     }
 
     try:
